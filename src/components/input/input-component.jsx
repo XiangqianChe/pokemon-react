b8c44ed865ee8679788667a8dfc585b32c380d5c
chexq <chexq35@gmail.com>
2024-01-09 21:20:55 -0500

    pokemon icon filter done

---
+++ src/components/input/input-component.jsx
@@ -0,0 +1,13 @@
+const Input = ({ onChangeHandler }) => {
+    return (
+        <input type="search" onChange={onChangeHandler} />
+    );
+};
+/*class Input extends React.Component {
+    render() {
+        const { onChangeHandler } = this.props;
+        return (
+            <input type="search" onChange={onChangeHandler} />
+        );
+    }
+}*/
